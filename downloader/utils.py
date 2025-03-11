3f9d37976a6dbaab2ab2170ce3b457b76422b63a
daniv <danivego2015@gmail.com>
2025-03-11 15:03:48 -0500

    Add create_directory function and NotFoundException class with documentation

--- downloader/utils.py
+++ downloader/utils.py
@@ -1,6 +1,10 @@
 import os
 
-
+"""
+Function that creates a directory
+@param directory: Directory path
+@return: None
+"""
 def create_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -19,3 +23,12 @@ def delete_directory(directory):
             elif os.path.isdir(file_path):
                 delete_directory(file_path)
         os.rmdir(directory)
+
+"""
+Exception for Not Found Errors in requests
+Handles 404 errors
+"""
+class NotFoundException(Exception):
+    """Custom Exception for Not Found Errors in requests"""
+    def __init__(self, message):
+        super().__init__(message)
