c5193947b65ec3c0fcfc9265f283dd04f5608f5b
daniv <danivego2015@gmail.com>
2025-03-11 21:46:09 -0500

    Improve docstrings for HTML extension check and directory creation functions; update NotFoundException class documentation.

--- extract_info/utils.py
+++ extract_info/utils.py
@@ -1,30 +1,30 @@
 import os
 from config import HTML_EXTENSION
 
-"""
-Function to check if the file has the HTML extension
-@param file: Name of the file
-@return: True if the file has the HTML extension, False otherwise
-"""
+
 def check_html_extension(file):
+    """
+    Function to check if the file has the HTML extension.
+    
+    :param file: Name of the file
+    :return: True if the file has the HTML extension, False otherwise
+    """
     return HTML_EXTENSION.match(file)
 
-"""
-Function to create a directory
-@param directory: Name of the directory
-@return: None
-"""
+
 def create_directory(directory):
+    """
+    Function to create a directory.
+    
+    :param directory: Name of the directory
+    :return: None
+    """
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
 
-"""
-Exception for Not Found Errors in requests
-Handles 404 errors
-"""
-class NotFoundException(Exception):
 
-    
-    """Custom Exception for Not Found Errors in requests"""
+class NotFoundException(Exception):
+    """Custom Exception for Not Found Errors in requests."""
+
     def __init__(self, message):
         super().__init__(message)
